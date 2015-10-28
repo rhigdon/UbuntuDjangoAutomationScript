1a2694b41be2e641d90106813d7bd52d37ff8ae6
rh <ryan.higdon@gmail.com>
2015-10-28 15:22:50 -0700

    update build script for uwsgi install and add sandboxing

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Overview
   1. Installs packages with apt-get
   2. creates a virtual environment for python and downloads python packages
   3. configure the django application
-  4. configure nginx for proxy
+  4. configure nginx for proxy with uwsgi
   5. update iptables
 
 ##Usage
@@ -17,3 +17,10 @@ Overview
   3. nginx
   4. django
   5. ssh
+
+##Stopping the WSGI Process
+By default the build script will start up the WSGI process.. If you need to make changes and restart you can kill the process like this:
+
+	uwsgi --stop /tmp/project-master.pid
+
+at this point you can just continue to use the build script to start the process again
--- build.sh
+++ build.sh
@@ -11,6 +11,12 @@ then
     git clone $REPOSRC 
 fi
 
+if [ ! -d $SANDBOX_DIR ]
+then
+    sudo mkdir /sandbox/
+    sudo chmod 777 /sandbox/
+fi
+
 #check if python virtual environment exists.. otherwise create it
 ENV_DIR=environment
 if [ ! -d $ENV_DIR ]
@@ -20,14 +26,14 @@ then
     python virtualenv-12.0.7/virtualenv.py environment
     rm virtualenv-12.0.7.tar.gz
     rm -rf virtualenv-12.0.7/
-    source environment/bin/activate
     pip install -r django-sample-app/requirements.txt
 else
     #If we already have an environment.. activate it
     # and make sure pip is up to date with the latest requirments
-    source environment/bin/activate
     pip install -r django-sample-app/requirements.txt
 fi
+
+source environment/bin/activate
 
 #Our sample app has a default template we need to replace
 #this is something that could be done with puppet/chef
@@ -38,7 +44,14 @@ sudo cp nginx.conf /etc/nginx/sites-enabled/default
 sudo cp myssl.cert /etc/ssl/certs/myssl.crt
 sudo cp myssl.key /etc/ssl/private/myssl.key
 
+sudo cp uwsgi_params /sandbox/
+sudo cp -r django-sample-app/static-assets/
+sudo cp -r django-sample-app/media/
+
+pip install uwsgi
+
 sudo service nginx restart
+uwsgi --ini projectname.ini --socket :8002 > /dev/null 2>&1 &
 
 #####Begin IP Tables Rules#######
 sudo iptables -P INPUT ACCEPT
